c5885f83c2d0d061f854da7404a16b0e888150c0
Fatih Beştaş <fatihbsts1@gmail.com>
2022-11-26 17:00:45 +0300

    Panteon Proje 2 Update 3
    Zeminin x eksenindeki hareket denklemi oluşturuldu.
    
    Engel yapısı oluşturuldu.

---
+++ Panteon-Akademi/Proje2-TappyFish/Assets/Scripts/GroundMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour
+{
+    public float speed;
+    BoxCollider2D box;
+    float groundWidth;
+    
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        groundWidth = box.size.x;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+
+        if(transform.position.x <= -(groundWidth * 1.5f))
+        {
+            transform.position = new Vector2(transform.position.x + 3 * groundWidth, transform.position.y);
+        }
+    }
+}
